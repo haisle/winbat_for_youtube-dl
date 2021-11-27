9da2577d55be60ed437de9153a4dbfd63523bef0
haisle <59172968+haisle@users.noreply.github.com>
2021-11-27 18:57:30 +0900

    Create youtube-dlv5_autobest_then_combined.bat

---
+++ youtube-dlv5_autobest_then_combined.bat
@@ -0,0 +1,23 @@
+@echo off
+:start
+echo;
+echo Please paste the URL.
+echo ctl + v to paste the URL.
+echo;
+set /p url=""
+echo;
+echo %url%
+youtube-dl -F --yes-playlist %url%
+echo;
+
+goto:both
+:both
+	youtube-dl -f bestvideo+bestaudio --yes-playlist %url% -o "%~dp0combined\%%(playlist)s\%%(title)s-%%(id)s.%%(ext)s"
+goto:fin
+:fin
+
+echo;
+echo Download finished! The video file was downloaded in current directory cimbined na folder
+echo You can download the another video OR if you close this window by ctr + c
+echo;
+goto:start
