0a43878981d4f5f1e7e1b634193e9075c4680645
haisle <59172968+haisle@users.noreply.github.com>
2021-12-16 20:30:15 +0900

    Create ytdlpv2_autobest_then_combined.bat
    update

---
+++ ytdlpv2_autobest_then_combined.bat
@@ -0,0 +1,25 @@
+@echo off
+:start
+echo;
+echo Please paste the URL.
+echo ctl + v to paste the URL.
+echo;
+set /p preurl=""
+rem this part remove timestamp t=xxx from %preurl%
+for /f "tokens=1 delims=&" %%a in ("%preurl%") do (set url=%%a)
+echo;
+echo %url%
+yt-dlp -F --yes-playlist %url%
+echo;
+
+goto:both
+:both
+	yt-dlp -f bestvideo+bestaudio --yes-playlist --write-thumbnail --merge-output-format mkv --download-archive "%~dp0combined\ytdlpdownloaded.txt" %url% -o "%~dp0combined\%%(playlist)s\%%(channel)s-%%(channel_id)s\%%(title)s-%%(upload_date)s-%%(id)s.%%(ext)s"
+goto:fin
+:fin
+
+echo;
+echo Download finished! The video file was downloaded in current directory cimbined na folder
+echo You can download the another video OR if you close this window by ctr + c
+echo;
+goto:start
